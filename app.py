eeb35f94210c0846646aed23ce5c28e3986e51fd
Thota Siva Teja <118587304+aideeplearner@users.noreply.github.com>
2023-01-20 10:53:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def predict():
     return render_template("home.html",prediction_text="the crop recommaded for you is {}".format(output))
     
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
